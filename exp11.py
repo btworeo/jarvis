d521d7a5454814cb1d906791142d0f4e2ef53418
xamarth <samarth1808kosta@gmail.com>
2025-12-14 17:55:18 +0530

    ax

--- exp11.py
+++
@@ -1,3 +0,0 @@
-Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
-print("Days of the week:", Days)
-print(type(Days))
--- mpl.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-plt.title("First Plot")
-plt.xlabel("X Label")
-plt.ylabel("Y Label")
-plt.show()
